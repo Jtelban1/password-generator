74844be47f7a1244b6fc55ad648f12169bfbaa22
John Telban <Jtelban1@gmail.com>
2020-02-06 14:02:09 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// Assignment Code
+
+var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.getElementById("generate");
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = lowercase.toUpperCase();
+var special = "!#%&'()*+,-./:;<=>?@[]^_`{|}~";
+var numbers = "123456789";
+
+// Write password to the #password input
+function writePassword() {
+  var params = {
+    lowercase: confirm("Would you like to use lowercase characters?"),
+    uppercase: confirm("Would you like to use uppercase characters?"),
+    specialChars: confirm("Would you like to use special characters?"),
+    numericChars: confirm("Would you like to use numeric characters?")
+  };
+
+  while (
+    params.length < 8 ||
+    params.length > 128 ||
+    params.length === undefined
+  ) {
+    params.length = parseInt(
+      prompt(
+        "How long would you like your password to be? Greater than 8 less than 128"
+      )
+    );
+  }
+
+  var password = generatePassword(params);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+function generatePassword(options) {
+  var password = "";
+
+  for (let index = 0; index < options.length; index++) {
+    if (options.uppercase) {
+      password += getRandomValue(uppercase);
+    }
+    if (options.lowercase) {
+      password += getRandomValue(lowercase);
+    }
+    if (options.specialChars) {
+      password += getRandomValue(special);
+    }
+    if (options.numericChars) {
+      password += getRandomValue(numbers);
+    }
+  }
+
+  return password.slice(0, options.length);
+
+  function getRandomValue(str) {
+    return str[Math.floor(math.random()) * str.length];
+  }
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
